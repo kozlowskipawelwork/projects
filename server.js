158f8ee768538614c107c72fc3b169e975c0b233
ESPP-ZDT <toplelfaget@gmail.com>
2022-07-04 19:33:57 +0200

    test

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req,res) => {
+    console.log("here")
+    res.render("index", { text :"World"})
+})
+
+const userRouter = require('./routes/users')
+
+app.use('/users', userRouter)
+
+
+app.listen(3000)
